2339cb58e90f40bf73008ed3054a6056addf1795
ranciaro <renan.ranciaro@gmail.com>
2020-08-20 14:54:32 -0300

    feat: added AI and bounds limit

---
+++ Assets/Scripts/BumperAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperAI : MonoBehaviour
+{
+    public float Velocity = 5f;
+    public Transform Ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Ball.localPosition.z < transform.localPosition.z)
+            transform.localPosition += new Vector3(0, 0, -Velocity * Time.deltaTime);
+        if (Ball.localPosition.z > transform.localPosition.z)
+            transform.localPosition += new Vector3(0, 0, Velocity * Time.deltaTime);
+    }
+}
